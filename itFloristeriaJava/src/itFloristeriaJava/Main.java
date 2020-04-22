3f923087b25a41717488a0ba7f1567f95959d2d9
Daniel Fernandez Fiaño <dffiano@gmail.com>
2020-04-22 12:54:50 +0200

    inicio main
    inicio creacion del proyecto

---
+++ itFloristeriaJava/src/itFloristeriaJava/Main.java
@@ -0,0 +1,12 @@
+package itFloristeriaJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int prueba = 0;
+		System.out.println(prueba);
+
+	}
+
+}
