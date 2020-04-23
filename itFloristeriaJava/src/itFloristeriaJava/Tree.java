2d5f378aeec13e752fcfdd59aae505de45a92ce2
Daniel Fernandez Fiaño <dffiano@gmail.com>
2020-04-23 10:28:08 +0200

    update
    añadir campos clase tree

--- itFloristeriaJava/src/itFloristeriaJava/Tree.java
+++ itFloristeriaJava/src/itFloristeriaJava/Tree.java
@@ -1,5 +1,26 @@
 package itFloristeriaJava;
 
 public class Tree {
+	private double height;
+
+	public Tree(double height,double price) {
+		super(price);
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "Tree [height=" + height + ", getHeight()=" + getHeight() + "]";
+	}
+	
+	
 
 }
